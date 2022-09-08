6ba8f2529657c20dde788ede5d247c0a945ad004
Emmanuel Christopher <adixchris@gmai.com>
2022-09-08 15:16:13 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - print the string in the put function
-*
-* Description: using the amin function
-* this program prints "with proper grammar, but the outcome is a piece of art"
-* Return: 0
-*/
-int main (void)
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art, \n")
-         return (0);
+	printf("with proper grammar, but the outcome is s piece of art, \n")
+	return (0);
 }
