2fc63a4e798d6edcee4d3f7baf5e42c01321b1fe
Emmanuel Christopher <adixchris@gmai.com>
2022-09-08 21:40:30 -0700

    task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- * main - print in the put function
  *
- * description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	getchar()
-	return 0;
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
