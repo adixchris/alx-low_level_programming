7fe757e38e11aa9119c8fa02d4e5cf8324617966
adixchris <adixchris@gmail.com>
2022-09-12 10:51:58 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- * 	  a comma followed by a space, in ascending order
+ * main - Prints the numbers from 00 to 99, numbers separated by a commas
  *
  * Return: Always 0
  */
